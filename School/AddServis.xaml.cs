2c0b941f76d4d55cbbd97f6370c972cde26a9151
Камиля Закирова <kamilyazakirova15935700@gmail.com>
2022-04-14 11:10:48 +0300

    fix

---
+++ School/AddServis.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace School
+{
+    /// <summary>
+    /// Логика взаимодействия для AddServis.xaml
+    /// </summary>
+    public partial class AddServis : Page
+    {
+        Service service = new Service();
+        public AddServis()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        private void btn_safe_Click(object sender, RoutedEventArgs e)
+        {
+            service.Title = tbx_name.Text;
+            service.Cost = int.Parse(tbx_cost.Text);
+            service.Description = tbx_full.Text;
+            service.Discount = int.Parse(tbx_discount.Text);
+            service.DurationInSeconds = int.Parse(tbx_duration.Text);
+
+            bd_connection.school.Service.Add(service);
+            bd_connection.school.SaveChanges();
+        }
+    }
+}
